83b0284aedf998e059f0300005eaa218bc1d2480
TmgGems <gyalwanglama123@gmail.com>
2023-11-30 18:26:25 +0545

    RockPaperScissor Game added.

---
+++ ConsoleApp2/RockPaperScissor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class RockPaperScissor
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int palyerPoints = 0,enemyPoints = 0;
+            Console.WriteLine("Welcome To The Rock Paper Scissor Game Station !!!");
+            Console.WriteLine("...............:)");
+            
+
+        }
+    }
+}
